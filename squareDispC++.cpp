21021409ce397dd983546bace90069effed1ab38
Sun Jay Yoo <sunjayyoo@sun-jays-imac.hwcampus.jhu.edu>
2017-05-17 12:15:52 -0400

    Final commit. C++ implementation complete.

--- squareDispC++.cpp
+++ squareDispC++.cpp
@@ -1,55 +1,55 @@
 #include <armadillo>
 #include <vector>
+#include <string>
 
 using namespace arma;
 
 int main (){
 
-	//display test
-	mat test;
-	test.load("test.csv",csv_ascii);
-	std::cout << "   x         y          z" << std::endl;
-	std::cout << test << std::endl;
-
-	//initialize test constants
+	//initialize track constants
 	int dt = 8;
 	double resolution = 0.107;
 
-	//throw error if dt too high
-	if (dt >= test.n_rows){
-		std::cout << "track length: " << test.n_rows << std::endl;
+	//load and display input track
+	mat track;
+	track.load("track.csv",csv_ascii);
+	std::cout << "         x         y          z" << std::endl;
+	std::cout << track << std::endl;
+
+	//throw error if dt is too high
+	if (dt >= track.n_rows){
+		std::cout << "track length: " << track.n_rows << std::endl;
 		std::cout <<"dt: " << dt << std::endl;
 		std::cout << "Time interval (dt) greater than track length" << std::endl;
 		return 0;
 	}
 
 	//create dt matrices
-	std::vector<mat> testOut;
-	testOut.push_back(mat(test.n_rows/dt+1, 7));
+	std::vector<mat> trackOut;
+	trackOut.push_back(mat(track.n_rows/dt+1, 7));
 	for (int i = 1; i < dt; i++){
-		testOut.push_back(mat(test.n_rows/dt, 7));
+		trackOut.push_back(mat(track.n_rows/dt, 7));
 	}
 
-
-	//fill in dt matrices
+	//fill and print in dt matrices
 	int counter = 0;
 	int j = 0;
 
-	for (int i = 0; i < test.n_rows; i++){
-			testOut[counter](j, 0) = test (i, 0);
-			testOut[counter](j, 1) = test (i, 1);
-			testOut[counter](j, 2) = test (i, 2);
-			testOut[counter](j, 3) = i + 1;
+	for (int i = 0; i < track.n_rows; i++){
+			trackOut[counter](j, 0) = track (i, 0);
+			trackOut[counter](j, 1) = track (i, 1);
+			trackOut[counter](j, 2) = track (i, 2);
+			trackOut[counter](j, 3) = i + 1;
 			if (i < dt){
-				testOut[counter](j, 4) = datum::nan;
-				testOut[counter](j, 5) = datum::nan;
-				testOut[counter](j, 6) = datum::nan;
+				trackOut[counter](j, 4) = datum::nan;
+				trackOut[counter](j, 5) = datum::nan;
+				trackOut[counter](j, 6) = datum::nan;
 			} else {
-				double dx = (test (i, 0)-test (i-dt, 0))*resolution;
-				double dy = (test (i, 1)-test (i-dt, 1))*resolution;
-				testOut[counter](j, 4) = dx*dx + dy*dy;
-				testOut[counter](j, 5) = dx;
-				testOut[counter](j, 6) = dy;
+				double dx = (track (i, 0)-track (i-dt, 0))*resolution;
+				double dy = (track (i, 1)-track (i-dt, 1))*resolution;
+				trackOut[counter](j, 4) = dx*dx + dy*dy;
+				trackOut[counter](j, 5) = dx;
+				trackOut[counter](j, 6) = dy;
 
 			}
 			counter++;
@@ -61,7 +61,7 @@ int main (){
 	for (int i = 0; i < dt; i++){
 			std::cout << "         x         y         z";
 			std::cout << "     index      disp        dx        dy" << std::endl;
-			std::cout << testOut[i] << "\n" <<std::endl;
+			std::cout << trackOut[i] << std::endl;
 	}
 
 	return 0;
---
+++ squareDispRcpp.R
@@ -0,0 +1,42 @@
+## squareDispRcpp.R (with squareDispRcpp.cpp dependency)
+##
+## Wu Lab, Johns Hopkins University
+## (Referenced squareDisp.R algorithm from Sheng Liu)
+##
+## Author: Sun Jay Yoo
+## Date: May 17, 2017
+
+
+#GCC GNU Fortran compiler required
+#TERMINAL COMMANDS:
+# $ curl -O http://r.research.att.com/libs/gfortran-4.8.2-darwin13.tar.bz2
+# $ sudo tar fvxz gfortran-4.8.2-darwin13.tar.bz2 -C /
+
+#Rcpp Armadillo required
+#R COMMANDS:
+# install.packages("RcppArmadillo")
+
+#Load required packages
+library(Rcpp)
+library(RcppArmadillo)
+
+#To load sample track data
+#library(smt)
+#folder=system.file("extdata","SWR1",package="smt")
+#trackll=readDiatrack(folder)
+#track = trackll[[1]][[3]]
+
+#Compile source C++ file, enter file path of file
+sourceCpp("/Users/sunjayyoo/Dropbox/Work/Particle\ Square\ Displacement/squareDispRcpp.cpp")
+
+#convert given track to matrix type
+track = data.matrix(track)
+
+#run squareDispRcpp.cpp 
+#(pass in an additional argument to change dt from the default of 1)
+#(pass in an another additional argument to change resolution from the default of 0.107)
+squareDispRcpp(track)
+squareDispRcpp(track, 2)
+squareDispRcpp(track, 8)
+squareDispRcpp(track, 9)
+
